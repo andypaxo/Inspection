19bdd73382ec377053d107041c4ba273563b5eb7
andypaxo <andy@andypaxo.net>
2011-02-27 19:16:52 -0700

    Save / restore form data using local storage.

--- inspection.js
+++ inspection.js
@@ -1,3 +1,32 @@
+(function($){
+	$.fn.populateFromJSON = function(data){
+		data = JSON.parse(data);
+		$.each(data, function(i, e) {
+			$('input[name="' + i + '"]').val(e);
+		});
+	};
+	
+	$.fn.serializeJSON=function() {
+		var data = {};
+		$.each($(this).serializeArray(), function(i, e) {
+			data[e.name] = e.value 
+		});
+		return JSON.stringify(data);
+	};
+})(jQuery);
+
 $(function() {
 	$('input.date').datepicker();
+	
+	$('form').submit(function(e) {
+		e.preventDefault();
+		
+		var formData = $(this).serializeJSON();
+		console.log(formData);
+		localStorage['inspection.header'] = formData;
+	});
+	
+	var loadedData = localStorage['inspection.header'];
+	if (loadedData != null)
+		$('form').populateFromJSON(loadedData);
 });
