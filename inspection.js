ce5bdec5528bd81a60844460692526e0e3c13115
andypaxo <andy@andypaxo.net>
2011-03-11 17:49:36 -0700

    Save and restore form values

--- inspection.js
+++ inspection.js
@@ -1,6 +1,17 @@
 $(function() {
-	inspection.generateFormFrom('FLHA');
+	var currentForm = localStorage['currentForm'];
+	if (currentForm)
+		inspection.generateFormFrom(JSON.parse(currentForm));
+	else
+		inspection.generateFormNamed('FLHA');
+	
+	
 	$('input.date').datepicker();
+	
+	$('form').submit(function(e) {
+		e.preventDefault();
+		localStorage['currentForm'] = $(this).serializeToJSON();
+	});
 	
 	$.fn.serializeToJSON=function() {
 		var data = inspection.currentTemplate;
@@ -14,21 +25,23 @@ $(function() {
 var inspection = {
 	templates : {},
 	currentTemplate : null,
-	generateFormFrom : function(templateName) {
+	generateFormNamed : function(templateName) {
+		this.generateFormFrom(this.templates[templateName]);
+	},
+	generateFormFrom : function(template) {
 		var fieldGenerators = {
 			text : function(label, name, value){
 				return $.tmpl(
-					'<div><dfn>${label}</dfn><input name="${name}">${value}</input></div>',
+					'<div><dfn>${label}</dfn><input name="${name}" value="${value}" /></div>',
 					{ label: label, name: name, value: value });
 			},
 			date : function(label, name, value) {
 				return $.tmpl(
-					'<div><dfn>${label}</dfn><input name="${name}" class="date">${value}</input></div>',
+					'<div><dfn>${label}</dfn><input name="${name}" class="date" value="${value}" /></div>',
 					{ label : label, name : name, value : value });
 			}
 		}
 		
-		var template = this.templates[templateName];
 		template.fields.forEach(function (field) {
 			$(fieldGenerators[field.type](field.label, field.name, field.value))
 				.prependTo('form');
