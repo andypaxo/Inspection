a4272f06c0e4ae1501f92984a0f2af53fc0a2042
andypaxo <andy@andypaxo.net>
2011-04-02 20:26:27 -0600

    Tests for all methods of form repository

--- test/formRepositoryTest.js
+++ test/formRepositoryTest.js
@@ -2,18 +2,42 @@ $(function() {
 	
 	var sut;
 	var storage;
+	var formname;
 	
+	function FakeFormNameGenerator() {
+		this.getName = function() {
+			return formname;
+		}
+	}
+
 	module(
 		"Form Repository",
 		{
 			setup: function(){
 				storage = {};
-				sut = new FormRepository(storage);
+				sut = new FormRepository(storage, new FakeFormNameGenerator());
 			}
 		}
 	);
 		
-	test("When saving a form", function() {
-		// Save form and check contents of storage
+	test("When getting the form listing", function() {
+		formname = "2011-04-03T02:12:44.536Z";
+		var formdate = new Date(formname).toDateString();
+		
+		var form = {name:"MyForm"};
+		sut.saveForm(form);
+		
+		var firstFoundForm = sut.getAll()[0];
+		equal(firstFoundForm.formName, formname, "List of all forms should contain saved form name");
+		equal(firstFoundForm.date, formdate, "List of all forms should contain saved form date");		
+	});
+	
+	test("When getting a form from storage", function() {
+		formname = "formname";
+		var form = {name:"MyForm"};
+		sut.saveForm(form);
+		
+		var foundForm = sut.loadForm(formname);
+		deepEqual(foundForm, form, "The form should be loaded");
 	});
 });
